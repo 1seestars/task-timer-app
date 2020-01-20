47ce9c8bc5c232bfa899fe8a69d9893a8a02b5b2
iseestars <edelweisskaty@gmail.com>
2020-01-21 01:47:28 +0200

    actions updated

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,6 +2,10 @@ export const CHANGE_TIMER = 'CHANGE_TIMER'
 export const GET_TASK_NAME = 'GET_TASK_NAME'
 export const CHANGE_SECONDS = 'CHANGE_SECONDS'
 export const RESET_COUNT = 'RESET_COUNT'
+export const CHANGE_TIMER = 'CHANGE_TIMER'
+export const GET_TASK_NAME = 'GET_TASK_NAME'
+export const CHANGE_SECONDS = 'CHANGE_SECONDS'
+export const RESET_COUNT = 'RESET_COUNT'
 
 export const changeTimer = () => ( {type: CHANGE_TIMER } )
 export const inputTaskName = newTaskName => ( {type: GET_TASK_NAME, payload: newTaskName} )
