fecbb38fc6ec917cb6ece3bfe66fd450ba9a66d4
iseestars <edelweisskaty@gmail.com>
2020-01-21 02:45:58 +0200

    styles updated

--- src/components/StartStopButton.jsx
+++ src/components/StartStopButton.jsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 
-export const StartStopButton = ({ action }) => <div id="task-btn"><Button id ="startStopButton" variant="contained" color="primary" onClick={action}>Start/Stop</Button></div>
+export const StartStopButton = ({ action }) => <div id="task-btn"><Button variant="contained" color="primary" onClick={action}>Start/Stop</Button></div>
